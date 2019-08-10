fe64fb15921bf902b419f274ee8f9ec4601bed7b
Vimal Sheoran <vimalpsheoran@gmail.com>
2019-08-10 18:53:34 +0530

    Made major changes. Implemented cleaner approach to do stuff.

--- samay.py
+++ samay.py
@@ -1,106 +1,125 @@
 from datetime import datetime,timedelta
 import re
 
+class Minute:
+
+	@classmethod
+	def start_of(cls, mtime):
+		return mtime.replace(second=0, microsecond=0)
+
+	@classmethod
+	def add(cls, mtime, val):
+		return mtime + timedelta(minutes=val)
+
+	@classmethod
+	def subtract(cls, mtime, val):
+		return mtime - timedelta(minutes=val)
+
+class Hour:
+
+	@classmethod
+	def start_of(cls, mtime):
+		return mtime.replace(minute=0, second=0, microsecond=0)
+
+	@classmethod
+	def add(cls, mtime, val):
+		return mtime + timedelta(hours=val)
+
+	@classmethod
+	def subtract(cls, mtime, val):
+		return mtime - timedelta(hours=val)
+
+class Day:
+
+	@classmethod
+	def start_of(cls, mtime):
+		return mtime.replace(hour=0, minute=0, second=0, microsecond=0)
+
+	@classmethod
+	def add(cls, mtime, val):
+		return mtime + timedelta(days=val)
+
+	@classmethod
+	def subtract(cls, mtime, val):
+		return mtime - timedelta(days=val)
+
 class Samay:
 
-        def __init__(self, mtime=None):
-            if mtime == None:
-                self.mtime = datetime.now()
-            else:
-                self.mtime = mtime
-            if not self.mtime:
-                raise Exception("Argument must be an instance of date time.")
+	def __init__(self, mtime=None):
+		if mtime == None:
+			self.mtime = datetime.now()
+		else:
+			self.mtime = mtime
+		if not self.mtime:
+			raise Exception("Argument must be an instance of date time.")
 
-        @classmethod
-        def from_str(cls, mtime):
-            mtime = re.sub("T.*Z", "", mtime)
-            return cls(mtime)
+	@classmethod
+	def from_int(cls, mtime):
+		try:
+			if len(str(mtime)) > 10:
+				mtime = mtime//1000
+			return cls(datetime.fromtimestamp(mtime))
+		except:
+			print("Argument must be of EPOC time.")
 
-        @classmethod
-        def from_int(cls, mtime):
-            try:
-                if len(str(mtime)) > 10:
-                    mtime = mtime//1000
-                return cls(datetime.fromtimestamp(mtime))
-            except:
-                print("Argument must be of EPOC time.")
+	@classmethod
+	def from_str(cls, mtime):
+		try:
+			f_mtime = mtime.split(":")
+			if len(f_mtime) == 2:
+				mtime = datetime.strptime(mtime, "%Y-%m-%d %H:%M")
+			else:
+				mtime = datetime.strptime(mtime, "%Y-%m-%d %H:%M:%S")
+			return cls(mtime)
+		except:
+			print("Argument must be of format YYYY-MM-DD HH:mm:ss or YYYY-MM-DD HH:mm")
 
-        @classmethod
-        def from_format(cls, mtime):
-            try:
-                f_mtime = mtime.split(":")
-                if len(f_mtime) == 2:
-                    mtime = datetime.strptime(mtime, "%Y-%m-%d %H:%M")
-                else:
-                    mtime = datetime.strptime(mtime, "%Y-%m-%d %H:%M:%S")
-                return cls(mtime)
-            except:
-                print("Argument must be of format YYYY-MM-DD HH:mm:ss or YYYY-MM-DD HH:mm")
+	def start_of(self, arg):
+		unit = self.get_time_unit(arg)
+		self.mtime = unit.start_of(self.mtime)
+		return self
 
-        def startOf(self,arg):
+	def add(self, val, _type):
+		unit = self.get_time_unit(_type)
+		self.mtime = unit.add(self.mtime, val)
+		return self
 
-                arg = str(arg).lower()
+	def subtract(self, val, _type):
+		unit = self.get_time_unit(_type)
+		self.mtime = unit.subtract(self.mtime, val)
+		return self
 
-                if arg == "min" or arg=="minute" or arg=="minutes" or arg=="m":
-                        self.mtime =  self.mtime.replace(second=0, microsecond=0)
-                elif arg == "hour" or arg=="hours" or arg=="h":
-                        self.mtime =  self.mtime.replace(minute=0, second=0, microsecond=0)
-                elif arg == "day" or arg=="days" or arg=="d":
-                        self.mtime =  self.mtime.replace(hour=0, minute=0, second=0, microsecond=0)
-                else:
-                        raise Exception('Argument 1 can be min,hour or day')
-                return self
+	def get_time_unit(self, arg):
+		if arg == "min":
+			return Minute
+		if arg == "hour":
+			return Hour
+		if arg == "day":
+			return Day
 
-        def add(self,val,time):
-                time = str(time).lower()
-                if not isinstance(val,int) :
-                        raise Exception('1st Argument must be a integer')
-                        return
+	def format(self,strArg):
+		strArg = str(strArg)
+		strArg = strArg.replace("YYYY","%Y")
+			.replace("DD","%d")
+			.replace("dd","%d")
+			.replace("mm","%M")
+			.replace("MM","%m")
+			.replace("ss","%S")
+			.replace("hh","%H")
+			.replace("HH","%H")
+		return self.mtime.strftime(strArg)
 
-                if time == "min" or time=="minute" or time=="minutes" or time=="m":
-                        self.mtime =  self.mtime + timedelta(minutes=val)
-                elif time == "hour" or time=="hours" or time=="h":
-                        self.mtime =  self.mtime + timedelta(hours=val)
-                elif time == "day" or time=="days" or time=="d":
-                        self.mtime =  self.mtime + timedelta(days=val)
-                else:
-                        raise Exception('Argument 2 can be min,hour or day')
-                
-                return self
+	def __str__(self):
+		return str(self.mtime.format("YYYY-MM-DD HH:mm"))
 
-        def sub(self,val,time):
-                time = str(time).lower()
-                if not isinstance(val,int) :
-                        raise Exception('1st Argument must be a integer')
-                        return
+	def unix(self):
+		return int(self.mtime.strftime("%s"))*1000
 
-                if time == "min" or time=="minute" or time=="minutes" or time=="m":
-                        self.mtime =  self.mtime - timedelta(minutes=val)
-                elif time == "hour" or time=="hours" or time=="h":
-                        self.mtime =  self.mtime - timedelta(hours=val)
-                elif time == "day" or time=="days" or time=="d":
-                        self.mtime =  self.mtime - timedelta(days=val)
-                else:
-                        raise Exception('Argument 2 can be min,hour or day')
-                
-                return self
+	def __gt__(self,other):
+		return self.mtime > other.mtime
 
-        def format(self,strArg):
-                strArg = str(strArg)
-                strArg = strArg.replace("YYYY","%Y").replace("DD","%d").replace("dd","%d").replace("mm","%M").replace("MM","%m").replace("ss","%S").replace("hh","%H").replace("HH","%H")
-                return self.mtime.strftime(strArg)
-
-        def __str__(self):
-                return str(self.format("YYYY-MM-DD HH:mm"))
-
-        def unix(self):
-                return int(self.mtime.strftime("%s"))*1000
-
-        def __gt__(self,other):
-                return self.mtime > other.mtime
-
-        def __eq__(self,other):
-                return self.mtime == other.mtime
-        def __lt__(self,other):
-                return self.mtime < other.mtime
+	def __eq__(self,other):
+		return self.mtime == other.mtime
+	def __lt__(self,other):
+		return self.mtime < other.mtime
 
